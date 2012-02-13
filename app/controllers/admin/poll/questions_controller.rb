679aaef52fcbf6ea54e545dbd58697ecc4b1600d
Oleg <oleg@theworkinggroup.ca>
2012-02-13 13:43:08 -0500

    admin controller, no views yet

--- app/controllers/admin/poll/questions_controller.rb
+++ app/controllers/admin/poll/questions_controller.rb
@@ -1,7 +1,65 @@
 class Admin::Poll::QuestionsController < Admin::Poll::BaseController
   
+  before_filter :build_question, :only => [:new, :create]
+  before_filter :load_question,  :only => [:show, :edit, :update, :destroy]
+  
   def index
     @questions = Poll::Question.all
+  end
+  
+  def show
+    render
+  end
+  
+  def new
+    render
+  end
+  
+  def edit
+    render
+  end
+  
+  def create
+    options = { }
+    (params[:question][:options] || []).each_with_index do |o, i|
+      options[i] = o
+    end
+    options.reject!{|k, v| v.blank?}
+    @question.options = options
+    @question.save!
+    flash[:notice] = 'Poll created'
+    redirect_to :action => :index
+  rescue ActiveRecord::RecordInvalid
+    flash.now[:error] = 'Failed to create Poll'
+    render :action => :new
+  end
+  
+  def update
+    @question.update_attributes!(params[:question])
+    flash[:notice] = 'Poll updated'
+    redirect_to :action => :index
+  rescue ActiveRecord::RecordInvalid
+    flash.now[:error] = 'Failed to update Poll'
+    render :action => :edit
+  end
+  
+  def destroy
+    @question.destroy
+    flash[:notice] = 'Poll deleted'
+    redirect_to :action => :index
+  end
+  
+protected
+  
+  def build_question
+    @question = Poll::Question.new(params[:question])
+  end
+  
+  def load_question
+    @question = Poll::Question.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:error] = 'Poll not found'
+    redirect_to :action => :index
   end
   
 end
--- test/functional/admin/poll/questions_controller_test.rb
+++ test/functional/admin/poll/questions_controller_test.rb
@@ -9,36 +9,90 @@ class Admin::Poll::QuestionsControllerTest < ActionController::TestCase
     assert assigns(:questions)
   end
   
+  def test_get_new
+    get :new
+    assert_response :success
+    assert_template :new
+    assert_select "form[action='/admin/poll/questions']"
+  end
+  
   def test_get_show
-    
+    get :show, :id => poll_questions(:default)
+    assert_response :success
+    assert_template :show
+    assert assigns(:question)
   end
   
   def test_get_show_failure
-    
+    get :edit, :id => 'invalid'
+    assert_response :redirect
+    assert_redirected_to :action => :index
+    assert_equal 'Poll not found', flash[:error]
   end
   
   def test_get_edit
-    
+    question = poll_questions(:default)
+    get :edit, :id => question
+    assert_response :success
+    assert_template :edit
+    assert assigns(:question)
+    assert_select "form[action='/admin/poll/questions/#{question.id}']"
   end
   
   def test_creation
-    
+    assert_difference 'Poll::Question.count' do
+      post :create, :question => {
+        :question => 'New Question',
+        :options  => ['Test Option 1', 'Test Option 2']
+      }
+      assert_response :redirect
+      assert_redirected_to :action => :index
+      assert_equal 'Poll created', flash[:notice]
+    end
   end
   
   def test_creation_failure
-    
+    assert_no_difference 'Poll::Question.count' do
+      post :create, :question => { }
+      assert_response :success
+      assert_template :new
+      assert_equal 'Failed to create Poll', flash[:error]
+    end
   end
   
   def test_update
+    question = poll_questions(:default)
+    put :update, :id => question, :question => {
+      :question => 'Updated Question'
+    }
+    assert_response :redirect
+    assert_redirected_to :action => :index
+    assert_equal 'Poll updated', flash[:notice]
     
+    question.reload
+    assert_equal 'Updated Question', question.question
   end
   
   def test_update_failure
+    question = poll_questions(:default)
+    put :update, :id => question, :question => {
+      :question => ''
+    }
+    assert_response :success
+    assert_template :edit
+    assert_equal 'Failed to update Poll', flash[:error]
     
+    question.reload
+    assert_not_equal '', question.question
   end
   
   def test_destroy
-    
+    assert_difference 'Poll::Question.count', -1 do
+      delete :destroy, :id => poll_questions(:default)
+      assert_response :redirect
+      assert_redirected_to :action => :index
+      assert_equal 'Poll deleted', flash[:notice]
+    end
   end
   
 end
